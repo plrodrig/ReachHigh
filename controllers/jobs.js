dd7f7ca5c02e3cae612a3905714d47141a85a47f
Priscilla Rodriguez <plrodrig@usc.edu>
2015-12-02 13:01:14 -0800

    Renders Glassdoor JSON

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,7 +1,16 @@
 angular
   .module('itunes')
-  .controller('JobsController', function(jobs){
+  .controller('JobsController', function(jobs, Glassdoor){
     console.log(jobs);
     var vm =  this;
     vm.jobs = jobs;
+    vm.search = function() {
+      console.log("DERP");
+      //modify glassdoor
+      Glassdoor.findAllJobs().then(function(songs) {
+        console.log(songs);
+        vm.jobs = songs;
+        //clear out data structure&textbox
+      });
+    };
   });
--- services/glassdoor.js
+++ services/glassdoor.js
@@ -11,16 +11,20 @@ angular
     // var useragent = 'Mozilla/5.0';
     var useragent = 'Mozilla/%2F4.0';
      var url = base + 't.p=' + tp + '&t.k=' + tk + '&userip=' + ip + '&useragent=' + useragent +
-     '&format=json&v=1&action=jobs-stats&returnStates=true&admLevelRequested=1&callback=JSON_CALLBACK';
+     //'&format=json&v=1&action=jobs-stats&returnStates=true&admLevelRequested=1&callback=JSON_CALLBACK';
+     '&format=json&v=1&action=jobs-prog&countryId=1&jobTitle=software&callback=JSON_CALLBACK';
 
      return $http.jsonp(url).then(function(response) {
-        return response.data;
+        return response.data.response;
      });
    }//end findAllJobs function
+   
  };
 });
 /*http://api.glassdoor.com/api/api.htm?t.p=48886&t.k=eedS6gVRSCm&userip=192.168.43.42&useragent=&format=json&v=1&action=jobs-stats&admLevelRequested=1&returnEmployers=true&returnJobTitles=true&city=los-angeles(/)
 /*
+
+http://api.glassdoor.com/api/api.htm?t.p=48886&t.k=eedS6gVRSCm&userip=0.0.0.0&useragent=&format=json&v=1&action=jobs-prog&countryId=1&jobTitle=software
 
 http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=48886&t.k=eedS6gVRSCm&action=employers&q=pharmaceuticals&userip=192.168.43.42&useragent=Mozilla/%2F4.0
 
